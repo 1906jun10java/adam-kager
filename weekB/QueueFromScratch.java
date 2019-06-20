8a737eae770a498502ab9e46ba3aa3408b39c4d6
Adam Kager <atksource>
2019-06-20 14:25:37 -0400

    for transfer 2

--- weekB/QueueFromScratch.java
+++ weekB/QueueFromScratch.java
@@ -1,27 +1,22 @@
-import java.util.Arrays;
-import java.util.Queue;
 
 public class QueueFromScratch {
 
 	public static void main(String[] args) {
-	
-//		Queue<Integer> q = (Queue<Integer>) new Arrays();
-		
+		// declared values
 		int[] aRay =  {1,2,3,4,5};
 		int pushNum = 6;
 		
-		
+		// Hard coded operations
 		int[] bRay = pushQueue(aRay, pushNum);
 		queuePrint(bRay);
-
+		System.out.println(" ");
+		int[] cRay = popQueue(bRay);
+		queuePrint(cRay);
+		System.out.println(" ");
+		peekQueue(cRay);
 	}
 	
-//	public static void queueBuilder(int[] ray) {
-//		
-//		for (int i=0; )		
-//	}
-	
-
+	// This just prints out the Queue
 	public static void queuePrint(int[] tempArray) {
 		for(int i = 0; i < tempArray.length; i++) {
 			System.out.print(tempArray[i] + ", ");
@@ -30,10 +25,18 @@ public class QueueFromScratch {
 	}
 	
 	// "pops" a number from the queue and removes it from the queue
-	public static int popQueue() {
-		return 0;
+	public static int[] popQueue(int[] bRay) {
+		int pop = bRay[0];
+		System.out.println("Oldest Element:  " + pop);
+		int[] tempArray = new int[bRay.length - 1];
+		for(int i = 0; i < tempArray.length; i++) {
+			tempArray[i] = bRay[i+1];
+		}
+		bRay = tempArray;
+		return bRay;
 	}
 	
+	// "pushes" a number to the queue in the 'newest' spot
 	public static int[] pushQueue(int[] aRay, int pushNum) {
 		int[] tempArray = new int[aRay.length + 1];
 		for(int i = 0; i < tempArray.length -1; i++) {
@@ -42,4 +45,11 @@ public class QueueFromScratch {
 		tempArray[tempArray.length - 1] = pushNum;
 		return tempArray;
 	}
+	
+	// "peeks" at the top or next element in the queue
+	public static void peekQueue(int[] cRay) {
+		System.out.println("Next Element " + cRay[0]);
+	}
+	
+	
 }

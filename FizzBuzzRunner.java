6a822672c3d7bb7da28021a7cee8cb9db39838ca
Adam Kager <atksource>
2019-06-28 16:00:17 -0400

    maybe a mistke

--- FizzBuzzRunner.java
+++ FizzBuzzRunner.java
@@ -1,20 +1,53 @@
 
+import java.util.Scanner;
+
 public class FizzBuzzRunner {
 
 	public static void main(String[] args) {
+		String userName = getUserName();
+		
 		int start = 1;
 		int finish = 100;		
+//		int start = aquireStarter();
+//		int finish = aquireFinisher();		
 		int[] aRay = {1, 3, 5, 7, 9, 11, 13, 15};
 		String[] bRay = {"Something", "Fizz", "Else", "Buzz", "Here"};
-		String userName = "Your Name Here";
 		FizzBuzz fOne = new FizzBuzz();
 		
 		
 		fOne.startProgram();
 //		fOne.fizzBuzzBasic(start, finish);
-		fOne.fizzBuzzAdvanced(start, finish, aRay, bRay);		
+		fOne.fizzBuzzAdvanced(start, finish, aRay, bRay);	
 		fOne.endProgram(userName);
 
+
 	}
+	
+	public static String getUserName() {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Hello, User.  May I have your name?");
+		String userName = s.next();	
+		s.close();
+		return userName;
+	}
+	//TODO find out why this throws java.util.NoSuchElementException
+//	public static int aquireStarter() {
+//		Scanner s = new Scanner(System.in);
+//		System.out.println("Please input your starting integer:  -If you wish to exit the "
+//				+ "program, please type something other than an integer");
+//		//TODO ~it is not a bug, it is a feature!  Figure out how to test if input is an integer  ...again
+//		int start = s.nextInt();	
+//		s.close();
+//		return start;
+//	}
+//	
+//	public static int aquireFinisher() {
+//		Scanner s = new Scanner(System.in);
+//		System.out.println("Please input your ending integer:  -If you wish to exit the "
+//				+ "program, please type something other than an integer");
+//		int finish = s.nextInt();	
+//		s.close();
+//		return finish;
+//	}
 
 }
--- weekB/Node.java
+++
@@ -1,7 +0,0 @@
-
-public class Node {
-	String str = "a";
-	int i;
-	int ref = 0;
-	
-}

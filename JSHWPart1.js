a921698beb9d97981a8bfa9ee1041bfd25f02fc9
Adam Kager <atksource>
2019-07-15 16:53:29 -0400

    JSHWPart1 Final Push

--- JSHWPart1.js
+++ JSHWPart1.js
@@ -3,10 +3,14 @@ window.onload = function() {
         let nth = document.getElementById("Nth").value
         let a = 1;
         let b = 2;
-        let k = languageOfNature(nth);
-        alert("Mathmatics is the language of nature " + k);
+        let aRay = languageOfNature(nth);
+        alert("Mathmatics is the language of nature " + aRay);
+        sortFibonacci(aRay);
+        factory(nth)
+        /*
         let j = sortFibonacci(k);
         alert("Sorted " + j);
+        */
     })
 }
 
@@ -27,20 +31,46 @@ function languageOfNature(nth) {
     return aRay;
 }
 
-//TODO work at this more, it is not working and the logic may (mostlikely) is flawed
+
 function sortFibonacci(aRay) {
-    let bRay = []
-    for(i = 0; i < aRay.size(); i++) {
-        for(j = 1; j < aRay.size(); j++) {
-            if (aRay[j] < aRay[i]) {
-                bRay.push(aRay[i]);
-            } else {
+    let bRay = [];
+    m = 0;
+    n = 0;
+    for(i = 0; i < aRay.length; i++) {
+        for(j = 1; j < aRay.length; j++) {
+            if (aRay[j] > aRay[i]) {
                 bRay.push(aRay[j]);
+                // aRay.splice(j, 1);
+                // //Swap only if j is bigger
+                // m = j;
+                // n = j - 1;
+                // while(m > 0) {
+                //     bRay.push(aRay[0]);
+                //     aRay.shift();
+                //     m--;
+                // }
+                // aRay.push(hold);
+                // while(n > 0)
+                //     aRay.push(bRay[0]);
+                //     bRay.shift();
             }
         }
     }
+    alert("This is your sorted Array:  " + bRay)
     return bRay;
 
+}
+//TODO:  -Results in an infinite loop
+function factory(n) {
+    while (n > 0) {
+        if (n != 1) {
+            j = j + n;
+            n--;
+            factory(n);
+        } else {
+            alert("Factoral of your entry is:  " + j);
+        }
+    }
 }
 
 

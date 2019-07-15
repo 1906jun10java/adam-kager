5b4a09127818024803271632f5fa725decc0d15c
Adam Kager <atksource>
2019-07-15 07:45:12 -0400

    Working on Sort

--- JSHWPart1.js
+++ JSHWPart1.js
@@ -1,33 +1,48 @@
 window.onload = function() {
     document.getElementById("answerAlert").addEventListener("click", function() {
-
+        let nth = document.getElementById("Nth").value
         let a = 1;
         let b = 2;
-        let k = languageOfNature();
+        let k = languageOfNature(nth);
         alert("Mathmatics is the language of nature " + k);
-
+        let j = sortFibonacci(k);
+        alert("Sorted " + j);
     })
-
-
-
 }
-//Pure Function
-function languageOfNature() {
+
+
+function languageOfNature(nth) {
     m = 0;
     n = 1;
     let aRay = [];
     aRay.push(m);
     aRay.push(n);
-    for(i = 0; i < 10; i++) {
+    for(i = 0; i < (nth -2); i++) {
         p = m + n;
         aRay.push(p);
         m = n;
         n = p;
-        console.log(aRay);
     }
 
     return aRay;
 }
+
+//TODO work at this more, it is not working and the logic may (mostlikely) is flawed
+function sortFibonacci(aRay) {
+    let bRay = []
+    for(i = 0; i < aRay.size(); i++) {
+        for(j = 1; j < aRay.size(); j++) {
+            if (aRay[j] < aRay[i]) {
+                bRay.push(aRay[i]);
+            } else {
+                bRay.push(aRay[j]);
+            }
+        }
+    }
+    return bRay;
+
+}
+
 
 
 
